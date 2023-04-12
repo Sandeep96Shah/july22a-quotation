70c12f5aff65cd810a531cc454fca2399255e7b4
Sandeep Shah <sandeep2016shah@gmail.com>
2023-04-12 18:44:15 +0600

    updated

--- config/monggose.js
+++ config/monggose.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-mongoose.connect("mongodb+srv://july22a:july22a@cluster0.wphqpiz.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGODB_URL)
 .then(() => {console.log("Connected to databse successfully!")})
 .catch((error) => {console.log("Error while connecting to the databse", error)})
